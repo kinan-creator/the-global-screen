9056aa0d6676ddce5a4482120d309ec0a9052a39
kinan-creator <k30488176@gmail.com>
2026-04-02 21:58:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const app = express();
 const path = require('path');
 
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
-app.get('*', (req, res) => {
-    const filePath = path.join(__dirname, req.path === '/' ? 'index.html' : req.path);
-    res.sendFile(filePath, (err) => {
-        if (err) res.sendFile(path.join(__dirname, 'index.html'));
-    });
-});
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'about.html')));
+app.get('/how-it-works', (req, res) => res.sendFile(path.join(__dirname, 'how-it-works.html')));
+app.get('/support', (req, res) => res.sendFile(path.join(__dirname, 'support.html')));
+app.get('/privacy', (req, res) => res.sendFile(path.join(__dirname, 'privacy.html')));
+app.get('/terms', (req, res) => res.sendFile(path.join(__dirname, 'terms.html')));
+app.get('/refund', (req, res) => res.sendFile(path.join(__dirname, 'refund.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
